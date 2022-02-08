1050d2956d2f0e2526f8406c4a3a43a067dc43dd
Ron Waller <ronwaller@Rons-Mac-mini.local>
2022-02-08 09:52:47 -0500

    Added README

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let axios = require('axios');
 // let express = require('express');
 // let path = require('path');
 let Twit = require('twit');
-// require('dotenv').config();
+require('dotenv').config();
 
 // let app = express();
 
@@ -59,7 +59,7 @@ getDadJoke()
   .then(res => console.log(res))
   .catch(err => console(err));
 
-process.exit();
+// process.exit();
 // setInterval(() => {
 //   let hour = new Date().getHours();
 //   if (hour = 12) {
